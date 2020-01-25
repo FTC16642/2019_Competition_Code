5041f538a7fbe2e585cec8369288afe12c1f1671
Neha Parthasarathy <oneninnprsquared@gmail.com>
2020-01-24 20:53:14 -0500

    added other simple autonomous

---
+++ SkyStone/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLeftNear.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import java.util.Locale;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.Common.HardwareIO;
+import org.firstinspires.ftc.teamcode.Grabber;
+import org.firstinspires.ftc.teamcode.Hook;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Sensors.BrickColorSensor;
+import org.firstinspires.ftc.teamcode.Sensors.DistSensor;
+
+@Autonomous(name="BlueLeftNear", group="Linear Opmode")
+
+public class BlueLeftNear extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private Grabber grabber = null;
+    private MecanumDrive drive = null;
+    private BrickColorSensor colorSensor = null;
+    private DistSensor backDistSensor = null;
+    private DistSensor sideDistSensor = null;
+    private Hook hook = null;
+    private boolean bFoundSky = false;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        HardwareIO IO = new HardwareIO(hardwareMap, gamepad1, gamepad2, telemetry);
+        
+        grabber = new Grabber(IO);
+
+        drive = new MecanumDrive(IO);
+
+        drive.initAuto();
+        grabber.init();
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+        
+        grabber.up();
+         drive.moveForward(-14 , drive.medium);
+
+ 
+    }
+
+    
+}
